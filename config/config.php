68a819e1d4532bdb637e16a98f1811dd6f09489a
siva2602 <160491264+sivakumar-saasforest@users.noreply.github.com>
2024-06-11 14:31:27 +0530

    config file added

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'rzq_api_url' => 'http://127.0.0.1:8000/api/v1/request'
+];
--- src/RZQPluginServiceProvider.php
+++ src/RZQPluginServiceProvider.php
@@ -6,18 +6,19 @@ use Illuminate\Support\ServiceProvider;
 
 class RZQPluginServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'rzq');
-    }
+  public function register()
+  {
+    $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'rzqpackage');
+  }
 
-    public function boot()
-    {
-        if ($this->app->runningInConsole()) {
+  public function boot()
+  {
+    if ($this->app->runningInConsole()) {
 
-            $this->publishes([
-                __DIR__ . '/../config/config.php' => config_path('rzq.php'),
-            ], 'config');
-        }
-    }
+        $this->publishes([
+          __DIR__.'/../config/config.php' => config_path('rzqpackage.php'),
+        ], 'config');
+    
+      }
+  }
 }
