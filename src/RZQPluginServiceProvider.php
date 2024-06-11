7708d45decde56db6187f4ff06404673191231fa
siva2602 <160491264+sivakumar-saasforest@users.noreply.github.com>
2024-06-11 15:12:43 +0530

    Main

--- src/RZQPluginServiceProvider.php
+++ src/RZQPluginServiceProvider.php
@@ -8,36 +8,41 @@ class RZQPluginServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'rzqpackage');
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        // $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'rzqpackage');
+        // $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-            $this->publishes([
-                __DIR__ . '/../config/config.php' => config_path('rzqpackage.php'),
-            ], 'config');
+        // $this->app->bind('task', function () {
+        //     return new Task();
+        // });
+        // if ($this->app->runningInConsole()) {
 
-            // Export the migration
-            if (!class_exists('CreatePlansTable')) {
-                $this->publishes([
-                    __DIR__ . '/../database/migrations/create_plans_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_plans_table.php'),
-                ], 'migrations');
-            }
+        //     $this->publishes([
+        //         __DIR__ . '/../config/config.php' => config_path('rzqpackage.php'),
+        //     ], 'config');
 
-            if (!class_exists('CreateShopsTable')) {
-                $this->publishes([
-                    __DIR__ . '/../database/migrations/create_shops_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_shops_table.php'),
-                ], 'migrations');
-            }
+        //     // Export the migration
+        //     if (!class_exists('CreatePlansTable')) {
+        //         $this->publishes([
+        //             __DIR__ . '/../database/migrations/create_plans_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_plans_table.php'),
+        //         ], 'migrations');
+        //     }
 
-            if (!class_exists('CreateChargesTable')) {
-                $this->publishes([
-                    __DIR__ . '/../database/migrations/create_charges_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_charges_table.php'),
-                ], 'migrations');
-            }
-        }
+        //     if (!class_exists('CreateShopsTable')) {
+        //         $this->publishes([
+        //             __DIR__ . '/../database/migrations/create_shops_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_shops_table.php'),
+        //         ], 'migrations');
+        //     }
+
+        //     if (!class_exists('CreateChargesTable')) {
+        //         $this->publishes([
+        //             __DIR__ . '/../database/migrations/create_charges_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_charges_table.php'),
+        //         ], 'migrations');
+        //     }
+        // }
     }
 }
