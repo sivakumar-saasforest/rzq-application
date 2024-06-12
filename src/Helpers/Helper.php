fec84b78f192ac966eddacddd900ab9682cce2cd
siva2602 <160491264+sivakumar-saasforest@users.noreply.github.com>
2024-06-12 15:11:31 +0530

    add helper.php file API route

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -2,7 +2,7 @@
 
 function rzq_api_site_url($storeId = null)
 {
-    return 'http://127.0.0.1:8000/api/v1/request/store-' . ($storeId ? $storeId : my_store_id());
+    return 'https://rzq-api.saasforest.com/api/v1/request/store-' . ($storeId ? $storeId : my_store_id());
 }
 
 function my_store_id()
