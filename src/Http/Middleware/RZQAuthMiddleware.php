0622cc07987906466007134564ee3d8eed6073e8
siva2602 <160491264+sivakumar-saasforest@users.noreply.github.com>
2024-06-12 14:06:16 +0530

    Migration add service provider and middleware

--- src/Http/Middleware/RZQAuthMiddleware.php
+++ src/Http/Middleware/RZQAuthMiddleware.php
@@ -4,12 +4,39 @@ namespace RzqApplication\Plugin\Http\Middleware;
 
 use Closure;
 use RzqApplication\Plugin\Store\Store;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RZQAuthMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $store = Store::store();
+        if (!auth()->check()) {
+            $store = (object) json_decode(Store::store($request->access_store), true);
+
+            $status = $store->status ?? 0;
+
+            if ($status == 422) {
+                return response()->json($store,  $status);
+            } else if ($status == 404 ||  $status == 500 ||  $status == 401) {
+                return abort($status);
+            }
+
+            $user = User::where('shop_id', $request->access_store)->first();
+
+            if (!$user) {
+                $user = User::create([
+                    'name' => $store->data['name'],
+                    'email' => $store->data['email'],
+                    'password' => Hash::make($request->access_store),
+                    'shop_id' => $request->access_store
+                ]);
+            }
+
+            $guard = 'web';
+
+            auth()->guard($guard)->login($user);
+        }
 
         return $next($request);
     }
--- src/RZQPluginServiceProvider.php
+++ src/RZQPluginServiceProvider.php
@@ -3,44 +3,24 @@
 namespace RzqApplication\Plugin;
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Http\Kernel;
 use RzqApplication\Plugin\Http\Middleware\RZQAuthMiddleware;
 
 class RZQPluginServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        // $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'rzqpackage');
-        // $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+       
     }
 
-    public function boot()
+    public function boot(Kernel $kernel)
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
         $router = $this->app->make(Router::class);
+        $kernel->appendMiddlewareToGroup('web', RZQAuthMiddleware::class);
         $router->aliasMiddleware('rzq-auth', RZQAuthMiddleware::class);
       
     }
-
-    // public function boot()
-    // {
-    //     // ... other things
-    //     $this->registerRoutes();
-    // }
-
-    // protected function registerRoutes()
-    // {
-    //     Route::group($this->routeConfiguration(), function () {
-    //         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-    //     });
-    // }
-
-    // protected function routeConfiguration()
-    // {
-    //     return [
-    //         'prefix' => config('blogpackage.prefix'),
-    //         'middleware' => config('blogpackage.middleware'),
-    //     ];
-    // }
 }
